26e0e4c607e00b8e549612fc6d57ffc47e462024
vm-shoeb <shoeb.merchant@purplepatch.in>
2023-05-06 14:43:06 +0530

    implementing View.OnClickListener on MainActivity Integrating OnLongClickListener on button Toast messages

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -6,8 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnHello:
+                Toast.makeText(this, "Hello Button Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnHello = findViewById(R.id.btnHello);
-        btnHello.setOnClickListener(new View.OnClickListener() {
+        btnHello.setOnClickListener(this);
+
+        btnHello.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View v) {
-                System.out.println("Hello");
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Long Press", Toast.LENGTH_LONG).show();
+                return true;
             }
         });
     }
